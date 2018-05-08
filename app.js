b4454c2420d6084752f61dfc1a13688c48830fc4
DoopDip <narit.su@icloud.com>
2018-05-09 01:33:11 +0700

    Insert new cd

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ Cd = require('./models/cd')
 mongoose.connect('mongodb://localhost:27017/cd')
 var db = mongoose.connection
 
+
 app.get('/', function (req, res) {
     res.sendfile('index.html')
 });
@@ -33,6 +34,24 @@ app.get('/api/buy/:id', function (req, res) {
         res.json(cd)
     })
 })
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.post("/api/add", function (req, res) {
+    console.log(req.body.cd.name)
+    console.log(req.body.cd.price)
+    Cd.addCd(req.body.cd, function (err, cd) {
+        if (err) throw err
+        res.json(cd)
+    })
+    // Cd.addCd(req.body.name, req.body.price, req.body.type, req.body.stock, function (err, cd) {
+    //     if (err) throw err
+    //     res.json(cd)
+    // })
+});
 
 console.log(port);
 app.listen(port)
--- models/cd.js
+++ models/cd.js
@@ -30,3 +30,9 @@ module.exports.getCdByName = function (name, callback) { //ค้นหาข้
 module.exports.buyCdByName = function (name, callback) { //ลบจำนวน cd
     Cd.update({name: name}, {$inc: {stock: 1}}, callback)
 }
+
+module.exports.addCd = function (cd, callback) { //เพิ่ม cd
+    console.log(cd.name)
+    Cd.create({name: cd.name, price: cd.price, type: cd.type, stock: cd.stock}, callback)
+    // Cd.create({name: name, price: price, type: type, stock: stock}, callback)
+}
